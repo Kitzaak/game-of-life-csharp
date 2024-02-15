82514a9c67e2a51292077cb56545dad236f4a3ff
Travis Unruh <travis.unruh@gmail.com>
2024-02-15 00:02:19 -0600

    Cells collection started, 4 tests

--- Cells.cs
+++ Cells.cs
@@ -1,24 +1,84 @@
 using System.Collections.Generic;
+using Xunit.Sdk;
 
 namespace game_of_life;
 
 public class Cells
 {
-    List<Tuple> _cells;
+    List<Cell> _cells;
     public Cells()
     {
-        _cells = new List<Tuple>();
+        _cells = new List<Cell>();
+    }
+
+    public void Load(List<Cell> cells_to_add)
+    {
+        _cells.Clear();
+        foreach(Cell cell in cells_to_add)
+        {
+            _cells.Add(cell);
+        }
+    }
+
+    public int Count => _cells.Count;
+    public bool Contains(Cell cell)
+    {
+        foreach(Cell _cell in _cells)
+        {
+            if(_cell.X == cell.X && _cell.Y == cell.Y)
+                return true;
+        }
+        return false;
+    }
+
+    public void Mutate(Mutation mutation)
+    {
+        foreach(Cell _cell in _cells)
+        {
+            if(_cell.X == mutation.Cell.X && _cell.Y == mutation.Cell.Y)
+            {
+                if(mutation.Action == CellAction.Die)
+                    _cells.Remove(_cell);
+                return;
+            }
+        }
     }
 }
 
-public class Tuple
+public class Cell
 {
-    int _x;
-    int _y;
+    readonly int _x;
+    readonly int _y;
 
-    public Tuple(int x = 0, int y = 0)
+    public Cell(int x = 0, int y = 0)
     {
         _x = x;
         _y = y;
     }
+
+    public int X { get => _x; }
+    public int Y { get => _y; }
 }
+
+public class Mutation
+{
+    readonly Cell _cell;
+    readonly CellAction _action;
+
+    public Mutation(Cell cell, CellAction action = CellAction.Flip)
+    {
+        _cell = cell;
+        _action = action;
+    }
+
+    public Cell Cell { get => _cell; }
+    public CellAction Action { get => _action; }
+}
+
+public enum CellAction
+{
+    Flip,
+    Die,
+    Live
+}
+
--- Tests/CellsTests.cs
+++ Tests/CellsTests.cs
@@ -5,8 +5,70 @@ namespace game_of_life;
 public class CellsTests
 {
     [Fact]
-    void can_create_cells_list()
+    void Can_create_cells_list_and_it_is_empty()
     {
-        Cells cells = new Cells();
+        var cells = new Cells();
+
+        Assert.Equal(0, cells.Count);
+    }
+
+    [Fact]
+    void Can_load_cells()
+    {
+        var cells = new Cells();
+
+        var cell_to_add = new List<Cell>
+            {
+                new(1,2),
+                new(2,2),
+                new(3,3)
+            };
+
+        cells.Load(cell_to_add);
+
+        Assert.Equal(cell_to_add.Count, cells.Count);
+    }
+
+    [Fact]
+    void Can_check_if_cell_is_alive()
+    {
+        var cells = new Cells();
+
+        var cell_to_add = new List<Cell>
+            {
+                new(1,2),
+                new(2,2),
+                new(3,3)
+            };
+
+        cells.Load(cell_to_add);
+
+        Assert.True(cells.Contains(cell_to_add[0]));
+        Assert.True(cells.Contains(cell_to_add[1]));
+        Assert.True(cells.Contains(cell_to_add[2]));
+
+        Assert.False(cells.Contains(new(0,0)));
+    }
+
+    [Fact]
+    void Add_remove_live_cells()
+    {
+        var cells = new Cells();
+
+        var cell_to_add = new List<Cell>
+            {
+                new(1,2),
+                new(2,2),
+                new(3,3)
+            };
+
+        cells.Load(cell_to_add);
+        var dying_cell = new Cell(2, 2);
+
+        Assert.True(cells.Contains(dying_cell));
+        
+        cells.Mutate(new Mutation(dying_cell, CellAction.Die));
+
+        Assert.False(cells.Contains(dying_cell));
     }
 }
